5e72785f35c5c6a58354c78c78f21a0086bda342
Puyet Laura Aurélie <puyet@SVBBCF20170012.intranet.epfl.ch>
2017-10-02 15:00:37 +0200

    yolo 2

--- Neuron.hpp
+++ Neuron.hpp
@@ -5,17 +5,34 @@
 
 using namespace std;
 
+typedef  vector Time_Spike; //tableau dans lequel il serait top d'avoir les temps correspondants aux spikes
+
 class Neuron {
 	
 	private: 
-		
-		double memb_pot; 
-		double Spikes_numb; 
-		double t; 
-		double T; 
-	
-		//
+		//variables constantes :
+		double Vo; //potentiel inital 
+		double R; //résistance de la membrane
+		double h;//(temps/step) temporal évolution
+		double t_start (0);
+		double t_stop;
+		double N (1); //nbr total of neurons (pour l'instant 1)
+		double Ne = 0.8*N; //nbr excitated neurons 
+		double Ni = 0.2 *N; //nbr inhibitated neurons
+		double Ce = Cext; //nbr connection excitated
+		double Ci = 0.25 * Ce; // nbr connection inhibitated
+		double Cext;// nbr coonexion externals
+		double VEext, //external frequencies exci
+		double VIext; //external frequencies inhi
+		double tE_cst; //
+		double tI_cst; 
+		//Variables non cst:
+		double V; //potentiel => méthode
 		double Iext;
+		double t;
+		double n; //nbr step
+		
+		
 		
 	public:
 		
@@ -24,7 +41,8 @@ class Neuron {
 		//type??? + attributs ???
 		void update_state(double t, double T);
 		//create methode V(t) qui est le membrane pot????
-		void set_Iext(); 
+		void set_Iext(); // donne possibilité à l'user de chosir son Iext
+		
 
 };
 	
