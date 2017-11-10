6959f9af9b996d00ad08a4939f9643cbd08cfeac
puyet <32450372+puyet@users.noreply.github.com>
2017-11-10 23:11:15 +0100

    Delete CTestTestfile.cmake

--- build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/Desktop/myfiles/Git/Projet_Neuro/gtest/googletest
-# Build directory: /home/student/Desktop/myfiles/Git/Projet_Neuro/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
