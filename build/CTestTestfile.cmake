44519527f75b01c87e41f1f97aa1fb4d2cf38071
puyet <32450372+puyet@users.noreply.github.com>
2017-11-10 23:01:47 +0100

    Delete CTestTestfile.cmake

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/Desktop/myfiles/Git/Projet_Neuro
-# Build directory: /home/student/Desktop/myfiles/Git/Projet_Neuro/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(neuron_unittest "neuron_unittest")
-subdirs(gtest)
